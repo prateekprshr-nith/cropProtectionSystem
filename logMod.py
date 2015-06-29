e03151e126da674fce25d1c344be7afd0a08297d
prateek <prateekprshr@gmail.com>
2015-06-29 15:13:43 +0530

    Functions declared not defined

---
+++ logMod.py
@@ -0,0 +1,34 @@
+__author__ = 'Prateek Prasher, CSED NITH'
+
+"""
+logMod - This is the module that defines the functions for logging various messages and saving the images
+The main functions that will be used directly in the system are:
+> saveImg()
+> writeLog()
+> writeErrLog()
+
+Other functions my be present here but they are solely defined for making the code clear, thus play no role individually
+and should not be invoked in the driver program.
+
+Friday 29 June, 2015
+v 1.0
+"""
+
+"""
+Image saving functions
+"""
+
+def saveImg(img):
+    pass
+
+#------------------------------------------------------------------#
+#------------------------------------------------------------------#
+
+def writeLog():
+    pass
+
+#------------------------------------------------------------------#
+#------------------------------------------------------------------#
+
+def writeErrLog(msg):
+    pass
